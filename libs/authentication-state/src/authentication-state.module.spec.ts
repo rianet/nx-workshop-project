df13cbdd51c7030b09f73e928d7aa42876192e0c
Justin Schwartzenberger <schwarty@gmail.com>
2017-12-01 10:51:20 -0800

    remove tests

--- libs/authentication-state/src/authentication-state.module.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthenticationStateModule } from './authentication-state.module';
-
-describe('AuthenticationStateModule', () => {
-  it('should work', () => {
-    expect(new AuthenticationStateModule()).toBeDefined();
-  });
-});
